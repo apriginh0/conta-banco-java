66aefb46bdca8a5390b9765d3f7414abce79baa1
apriginh0 <apriginh0@hotmail.com>
2024-07-11 00:15:53 -0300

    Revert "First commit"
    This reverts commit 6d2dc6858da26cdf27e18b57f984f3cf229bd123.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,35 +1,5 @@
-import java.util.Scanner;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.Locale;
-
 public class ContaTerminal {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Por favor, digite o número da Conta:");
-        int numero = scanner.nextInt();
-        scanner.nextLine();  // Limpar o buffer do scanner
-
-        System.out.println("Por favor, digite o número da Agência:");
-        String agencia = scanner.nextLine();
-
-        System.out.println("Por favor, digite o nome do Cliente:");
-        String nomeCliente = scanner.nextLine();
-
-        System.out.println("Por favor, digite o saldo:");
-        double saldo = scanner.nextDouble();
-
-        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.forLanguageTag("pt-BR"));
-        symbols.setDecimalSeparator(',');
-        symbols.setGroupingSeparator('.');
-
-        DecimalFormat df = new DecimalFormat("R$#,##0.00", symbols);
-        String saldoFormatado = df.format(saldo);
-
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
-                ", conta " + numero + " e seu saldo " + saldoFormatado + " já está disponível para saque.");
-
-        scanner.close();
+        System.out.println("Hello world!");
     }
 }
